f38700609a21f36b96dd9d31ae1fdb4825eef1d1
itschoolgomother <itschoolgomother@gmail.com>
2020-01-12 19:28:31 +0200

    12.1.2020

---
+++ vs6Артем/script.js
@@ -0,0 +1,10 @@
+var test = document.getElementById("box");
+var	colors = ["red","green","blue","indigo"];
+
+
+test.style.backgroundColor = colors[2];
+test.style.width = "100px";
+test.style.height = "100px";	
+
+alert("Hi");
+console.log("23342")
